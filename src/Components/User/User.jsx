6332c88b1c0d5fd3d0476970bfe6ae8deb430ecd
mini <jeongminhui0503@gmail.com>
2022-12-28 15:32:47 +0900

    nonnono do not merge

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import './User.scss';
-import Footer from '../Footer/Footer';
+import React from "react";
+import "./User.scss";
+import Footer from "../Footer/Footer";
 
 const User = () => {
-    return (
-        <div className='User'>
-            <h1>유저 페이지입니다</h1>
-            <Footer />
-        </div>
-    );
+    // 되나이?
+  return (
+    <div className="User">
+      <h1>유저 페이지입니다</h1>
+      <Footer />
+    </div>
+  );
 };
 
 export default User;
