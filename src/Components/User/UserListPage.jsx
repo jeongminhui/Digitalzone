06a451eb9868360f7f18dd246f57658981162385
“TAEWONgit config --global user.email “youbeen123@gmail.com <taewon@gimtaewon-ui-MacBookAir.local>
2022-12-29 09:18:48 +0900

    update UserDataCenter

---
+++ src/Components/User/UserListPage.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+const UserListPage = () => {
+  const { userList } = useContext(UserContext);
+
+  // 블트노 권한에 따른 OX 표시 함수 
+  const userAdmin = (data) => {
+    if (data === true) {
+      return "O";
+    } else {
+      return "X";
+    }
+  }
+  // 서비스 이용 개수 계산 함수
+//   const userService = (obj) => {
+//     const serviceArr = [obj]
+//     console.log(serviceArr);
+//   }
+
+  const userStatus = (data) => {
+    if (data === true) {
+        return "정상"
+    } else {
+        return "비정상"
+    }
+  }
+    
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>유형</th>
+            <th>이름</th>
+            <th>소속</th>
+            <th>이메일(아이디)</th>
+            <th>대시보드</th>
+            <th>블록</th>
+            <th>트랜잭션</th>
+            <th>노드</th>
+            <th>서비스</th>
+            <th>이용중인 서비스</th>
+            <th>등록일자</th>
+            <th>상태</th>
+            <th>삭제</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((user, idx) => (
+            <tr key={idx}>
+              <td>{user.id}</td>
+              <td>{user.userclass}</td>
+              <td>{user.username}</td>
+              <td>{user.userteam}</td>
+              <td>{user.userid}</td>
+              <td>{userAdmin(user.useradmin.dashboard)}</td>
+              <td>{userAdmin(user.useradmin.block)}</td>
+              <td>{userAdmin(user.useradmin.transaction)}</td>
+              <td>{userAdmin(user.useradmin.node)}</td>
+              <td>{userAdmin(user.useradmin.service)}</td>
+              {/* <td>{userService(user.userservice.service)}</td> */}
+              <td>2</td>
+              <td>{user.userdate}</td>
+              <td>{userStatus(user.userstatus)}</td>
+              <td><button>삭제</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserListPage;
