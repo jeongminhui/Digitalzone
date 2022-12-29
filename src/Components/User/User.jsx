4105618ab8e51163b79766775328b7326a3e11d2
mini <jeongminhui0503@gmail.com>
2022-12-29 09:33:31 +0900

    mini text modal do not merge plz nononono

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./User.scss";
 import Footer from "../Footer/Footer";
+import Modal from "./Components/Modal";
 
 const User = () => {
-    // 되나이?
   return (
     <div className="User">
       <h1>유저 페이지입니다</h1>
+      <Modal />
       <Footer />
     </div>
   );
