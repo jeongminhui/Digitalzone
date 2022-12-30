4480b195fcc15749811fdc5ed664132716792d88
jeongminhui <62429295+jeongminhui@users.noreply.github.com>
2022-12-30 11:18:47 +0900

    style : 깨짐 해결

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,11 +3,9 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div className="container">
       <div className="Footer">
         <div>Footer 영역입니다</div>
       </div>
-    </div>
   );
 };
 
