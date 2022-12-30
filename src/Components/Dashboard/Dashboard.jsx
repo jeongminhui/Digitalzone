0b11f7dd3832af0844859205b057ed7b94db29c8
jeongminhui <62429295+jeongminhui@users.noreply.github.com>
2022-12-30 11:15:41 +0900

    Update Dashboard.jsx

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,12 +4,10 @@ import Footer from "../Footer/Footer";
 
 const Dashboard = () => {
   return (
-    <div className="container">
       <div className="DashBoard">
         <h1>대시보드 페이지 입니다</h1>
         <Footer />
       </div>
-    </div>
   );
 };
 
