4fff977e3bc7c307b05b005a83b9772789c7d6c4
RYU339 <ryuryuryu339@likelion.org>
2023-01-03 14:14:39 +0900

    feat : 비밀번호 조건 설정 및 재확인 추가

--- src/Components/User/UserMyPageComponents/SignUp.jsx
+++ src/Components/User/UserMyPageComponents/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from '../../../firebase';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
