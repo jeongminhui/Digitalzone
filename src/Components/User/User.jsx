aa692253847d6bd38fb1d3a541daa3269b646c83
mini <jeongminhui0503@gmail.com>
2023-01-03 09:28:07 +0900

    docs : 주석 달음

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,16 @@ import Footer from "../Footer/Footer";
 import UserDataCenter from "./UserManagementComponents/UserDataCenter";
 import UserAddPage from "./UserAddPage";
 import UserMyPage from "./UserMyPageComponents/UserMyPage";
+import UserListPage from "./UserManagementComponents/UserListPage";
 
 const User = () => {
   return (
     <div className="User">
       {/* <UserDataCenter /> */}
-      <Footer />
+      {/* 파이어베이스 떄문인지 아직도 화면 안뜸. 그래서 주석처리함. */}
+      {/* <UserListPage /> */}
       <UserMyPage />
+      <Footer />
     </div>
   );
 };
