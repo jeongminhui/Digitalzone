c15f2b885d45700cdae804f138777dbac4e7a9a7
RYU339 <ryuryuryu339@likelion.org>
2023-01-05 09:45:36 +0900

    feat : user page test

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import "./User.scss";
-import Footer from "../Footer/Footer";
-import UserDataCenter from "./UserManagementComponents/UserDataCenter";
-import UserAddPage from "./UserAddPage";
-import UserMyPage from "./UserMyPageComponents/UserMyPage";
-import UserListPage from "./UserManagementComponents/UserListPage";
+import React from 'react';
+import './User.scss';
+import Footer from '../Footer/Footer';
+import UserDataCenter from './UserManagementComponents/UserDataCenter';
+import UserAddPage from './UserAddPage';
+import UserMyPage from './UserMyPageComponents/UserMyPage';
+import UserListPage from './UserManagementComponents/UserListPage';
 
 const User = () => {
-  return (
-    <div className="User">
-      {/* <UserDataCenter /> */}
-      {/* 파이어베이스 떄문인지 아직도 화면 안뜸. 그래서 주석처리함. */}
-      {/* <UserListPage /> */}
-      <UserMyPage />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='User'>
+            <UserDataCenter />
+            {/* 파이어베이스 떄문인지 아직도 화면 안뜸. 그래서 주석처리함. */}
+            <UserListPage />
+            {/* <UserMyPage /> */}
+            <Footer />
+        </div>
+    );
 };
 
 export default User;
