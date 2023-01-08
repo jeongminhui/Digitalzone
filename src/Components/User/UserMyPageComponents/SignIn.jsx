0a4de84498e2e514337f15a0fa27beb3bc70256c
mini <jeongminhui0503@gmail.com>
2023-01-09 01:41:03 +0900

    feat : Page 컴포넌트 구현 및 로그인 페이지 스타일링중

--- src/Components/User/UserMyPageComponents/SignIn.jsx
+++ src/Components/User/UserMyPageComponents/SignIn.jsx
@@ -1,79 +1,85 @@
-import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { db } from '../../../firebase';
-import { collection, deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { db } from "../../../firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 const SignIn = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const userCollection = collection(db, 'users');
-    const auth = getAuth();
+  const userCollection = collection(db, "users");
+  const auth = getAuth();
 
-    const clickHandler = async (e) => {
-        e.preventDefault();
-        await signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // 비동기로 데이터 가져오기
-                const dataPrint = async () => {
-                    const user = userCredential.user;
-                    const docRef = doc(userCollection, user.uid);
-                    const data = await getDoc(docRef);
-                    const userInfo = data.data();
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // 비동기로 데이터 가져오기
+        const dataPrint = async () => {
+          const user = userCredential.user;
+          const docRef = doc(userCollection, user.uid);
+          const data = await getDoc(docRef);
+          const userInfo = data.data();
 
-                    console.log(userInfo);
-                    alert(`${userInfo.username}님, 로그인되었습니다`);
-                };
-                dataPrint();
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorMessage);
-            });
-        setEmail('');
-        setPassword('');
-    };
+          console.log(userInfo);
+          alert(`${userInfo.username}님, 로그인되었습니다`);
+        };
+        dataPrint();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+    setEmail("");
+    setPassword("");
+  };
 
-    const deleteUser = async (e) => {
-        e.preventDefault();
-    };
+  const deleteUser = async (e) => {
+    e.preventDefault();
+  };
 
-    return (
+  return (
+    <div>
+      <form>
+        {/* <h1>로그인</h1> */}
         <div>
-            <form>
-                <h1>로그인</h1>
-                <div>
-                    아이디:
-                    <input
-                        type='email'
-                        className='signInEmail'
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    비밀번호:
-                    <input
-                        type='password'
-                        className='signInPassword'
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                    />
-                </div>
-                <button type='submit' className='SignInButton' onClick={clickHandler}>
-                    로그인
-                </button>
-            </form>
-            <button type='submit' className='deleteUser' onClick={deleteUser}>
-                사용자 삭제
-            </button>
+          {/* 아이디: */}
+          <input
+            type="email"
+            className="signInEmail"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
-    );
+        <div>
+          {/* 비밀번호: */}
+          <input
+            type="password"
+            className="signInPassword"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="SignInButton" onClick={clickHandler}>
+          로그인
+        </button>
+      </form>
+      {/* <button type="submit" className="deleteUser" onClick={deleteUser}>
+        사용자 삭제
+      </button> */}
+    </div>
+  );
 };
 
 export default SignIn;
