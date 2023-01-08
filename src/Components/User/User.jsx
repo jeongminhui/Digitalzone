855dbc30d95a560c41aa8c432ac191e5fb816a99
mini <jeongminhui0503@gmail.com>
2023-01-09 00:29:05 +0900

    feat : Modal 완성

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -3,10 +3,12 @@ import "./User.scss";
 import Footer from "../Footer/Footer";
 import UserDataCenter from "./UserManagementComponents/UserDataCenter";
 import UserMyPage from "./UserMyPageComponents/UserMyPage";
+import Modal1 from "../CommonComponents/Modal/Modal1";
 
 const User = () => {
   return (
     <div className="User">
+      <Modal1 buttonName="사용자 추가">과연?!</Modal1>
       <Footer />
     </div>
   );
