58066955fd6e43f61d255e93e8c61f76486e1989
Jaeeun <jaeeun9862@gmail.com>
2023-01-09 22:06:41 +0900

    feat : 대시보드 시간당 서비스 등록건수 차트 데이터 연결

--- src/Components/Dashboard/DashboardInfo/EnrollService.jsx
+++ src/Components/Dashboard/DashboardInfo/EnrollService.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ResponsiveLine } from "@nivo/line";
 import { Link } from "react-router-dom";
 
@@ -8,16 +8,63 @@ import { networkSelector } from "../../../Recoil/Selector";
 
 const EnrollService = () => {
   const networkData = useRecoilValue(networkSelector);
+  const [active, setActive] = useState("");
+  const [enroll, setEnroll] = useState([]);
+
+  // 활성네트워크만 필터링 (=active)
+  useEffect(() => {
+    async function getActive() {
+      const data = await networkData;
+      const dataFiltering = data.filter((item) => {
+        return item.ntwstatus === true;
+      });
+      setActive(dataFiltering);
+    }
+    getActive();
+  }, [networkData]);
+
+  // 활성 네트워크 중 enrollservice 값만 추출해서 함수로 넘겨주기
+  useEffect(() => {
+    async function getService() {
+      const data = await active;
+      data.map((item, idx) => {
+        return makeChartData(item.enrollservice);
+      });
+    }
+    getService();
+  }, [active]);
+
+  let enroll1 = 0;
+  let enroll2 = 0;
+  let enroll3 = 0;
+  let enroll4 = 0;
+  let enroll5 = 0;
+
+  const makeChartData = (item) => {
+    enroll1 += item["10:00"];
+    enroll2 += item["11:00"];
+    enroll3 += item["12:00"];
+    enroll4 += item["13:00"];
+    enroll5 += item["14:00"];
+
+    setEnroll({
+      time1: enroll1,
+      time2: enroll2,
+      time3: enroll3,
+      time4: enroll4,
+      time5: enroll5,
+    });
+  };
 
   const data = [
     {
       id: "시간당 서비스 등록건수",
       data: [
-        { x: "10:00", y: 300 },
-        { x: "11:00", y: 240 },
-        { x: "12:00", y: 320 },
-        { x: "13:00", y: 210 },
-        { x: "14:00", y: 345 },
+        { x: "10:00", y: enroll.time1 },
+        { x: "11:00", y: enroll.time2 },
+        { x: "12:00", y: enroll.time3 },
+        { x: "13:00", y: enroll.time4 },
+        { x: "14:00", y: enroll.time5 },
       ],
     },
   ];
@@ -30,7 +77,7 @@ const EnrollService = () => {
           <ResponsiveLine
             data={data}
             margin={{ top: 20, right: 15, bottom: 40, left: 45 }}
-            colors={{ scheme: "pastel1" }}
+            colors={["#008FFB"]}
             xScale={{ type: "point" }}
             yScale={{
               type: "linear",
@@ -59,7 +106,7 @@ const EnrollService = () => {
             enableCrosshair={false}
             useMesh={true} // MouseHover시 효과
             enableArea={true} //fill 효과
-            areaOpacity={0.45} //fill 효과 투명도
+            areaOpacity={0.3} //fill 효과 투명도
           />
         </div>
       </Link>
