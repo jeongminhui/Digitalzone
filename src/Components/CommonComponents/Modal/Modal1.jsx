7b4c5772b8ef3ed99159d6e4bcfa3ebc4109ce68
mini <62429295+jeongminhui@users.noreply.github.com>
2023-01-10 16:18:28 +0900

    Update Modal1.jsx
    불필요한 주석 삭제

--- src/Components/CommonComponents/Modal/Modal1.jsx
+++ src/Components/CommonComponents/Modal/Modal1.jsx
@@ -6,7 +6,7 @@ const Modal1 = (props) => {
   /* 민희 메이드 모달 서식 입니다! :)
   모달이 필요한 페이지에 이 모달을 import하시고
   속성에 buttonName = {} 로 버튼 이름 넣어 주시고
-  모달 안에 들어가야할 내용들은 import한 모달 의 자식 컴포넌트로 넣어주세요! 
+  모달 안에 들어가야할 내용들은 import한 모달의 자식 컴포넌트로 넣어주세요! 
 
   <Modal1> 이 안에 컴포넌트로 넣어주세요! :D </Modal1>
   */
@@ -26,7 +26,7 @@ const Modal1 = (props) => {
   const handleCancel = () => {
     setOpen(false);
   };
-  // 테스트
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
