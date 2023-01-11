3ffdbb305caa7a3a797b37e788fc1f0d68114294
RYU339 <ryuryuryu339@likelion.org>
2023-01-11 15:26:18 +0900

    feat : 삭제 예정

--- src/Components/User/UserPageComponents/UserAdd/UserAdd_Page.jsx
+++ src/Components/User/UserPageComponents/UserAdd/UserAdd_Page.jsx
@@ -1,9 +1,50 @@
-import React from "react";
 import UserAdd from "./Components/UserAdd";
+import UserAddModal from "./Components/UserAddModal";
+import React, { useState } from "react";
+import {
+  Button,
+  Cascader,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Switch,
+  TreeSelect,
+} from "antd";
 
 const UserAdd_Page = () => {
+  const [componentSize, setComponentSize] = useState("default");
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
   return (
     <div>
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+        <Form.Item label="Input">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Select">
+          <Select>
+            <Select.Option value="demo">Demo</Select.Option>
+          </Select>
+        </Form.Item>
+      </Form>
+      <hr />
       <UserAdd />
     </div>
   );
