965e20b9d14d1875fb9f0aeba78ae187fa09d01b
Jaeeun <jaeeun9862@gmail.com>
2023-01-12 11:10:12 +0900

    style : NotFound 페이지 스타일 수정

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -6,11 +6,15 @@ const NotFound = () => {
   const navigate = useNavigate();
   return (
     <div className="NotFound">
-      <div className="NotFound_img">이미지구역</div>
       <div className="NotFound_text">
-        <h1>404</h1>
-        <h3>Page NotFound!</h3>
+        <h1>잘못된 접근입니다</h1>
         <button onClick={() => navigate("/")}>메인으로 돌아가기</button>
+      </div>
+      <div className="NotFound_img">
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/NotFound.svg`}
+          alt="notFound"
+        />
       </div>
     </div>
   );
