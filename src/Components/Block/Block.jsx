f12380379be1590e809aa6b4b17a41aebf033064
gyull <kuri6233@gmail.com>
2023-01-16 20:13:19 +0900

    test

--- src/Components/Block/Block.jsx
+++ src/Components/Block/Block.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Block.scss";
 import Footer from "../Footer/Footer";
 import { useNavigate } from "react-router-dom";
